19814cea2d8371c60799c2f65a819c2d002835da
curlyZafir <109031346+curlyZafir@users.noreply.github.com>
2023-03-07 18:06:59 +0100

    mise a jour

--- théorique/entrainement/assets/js/question.js
+++ théorique/entrainement/assets/js/question.js
@@ -196,174 +196,210 @@ let questions = [
     //     ],
     //     explanation : "Avant d'effectuer un changement de direction vers la droite, vous devez serrer le plus possible le bord droit de la chaussée. Quitter le rond-point depuis cette bande risque de créer un conflit de trajectoire avec un conducteur circulant dans la bande de droite et qui ne compte pas quitter le rond-point à cette sortie, ce qui est parfaitement son droit."
     // },
-    {
-        numb: 16,
-        points: 1,
-        question : "Quand ma voiture consommera-t-elle le plus de carburant ?",
-        image : '<img src="../../assets/image/entrainement-image/quiz1/voiture-vitres-ouvertes.jpg">',
-        answer: "En circulant vitres ouvertes",
-        options: [
-            "Ouvertes ou fermées, aucune influence sur la consommation",
-            "En circulant vitres fermées",
-            "En circulant vitres ouvertes"
-        ],
-        explanation : "Rouler vitres ouvertes perturbe l'aérodynamisme au détriment de votre consommation."
-    },
-    {
-        numb: 17,
-        points : 1,
-        question : "une question ?",
-        image : "",
-        answer: "2eme reponse",
-        options: [
-            "1ere reponse stv",
-            "2eme reponse",
-            "3eme reponse"
-        ]
-    },
+    // {
+    //     numb: 16,
+    //     points: 1,
+    //     question : "Quand ma voiture consommera-t-elle le plus de carburant ?",
+    //     image : '<img src="../../assets/image/entrainement-image/quiz1/voiture-vitres-ouvertes.jpg">',
+    //     answer: "En circulant vitres ouvertes",
+    //     options: [
+    //         "Ouvertes ou fermées, aucune influence sur la consommation",
+    //         "En circulant vitres fermées",
+    //         "En circulant vitres ouvertes"
+    //     ],
+    //     explanation : "Rouler vitres ouvertes perturbe l'aérodynamisme au détriment de votre consommation."
+    // },
+    // {
+    //     numb: 17,
+    //     points : 1,
+    //     question : "Suis-je sur la bonne voie pour me rendre vers Paris ?",
+    //     image : '<img src="../../assets/image/trafic/X3542.jpg">',
+    //     answer: "Oui",
+    //     options: [
+    //         "Non, il faudra tourner a droite",
+    //         "Oui"
+    //     ],
+    //     explanation : "Le panneau indique bien que je peux être sur les trois voie pour prendre cette direction"
+    // },
     // {
     //     numb: 18,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Je souhaite tourner a droite, suis-je prioritaire pour pouvoir passer ?",
+    //     image : '<img src="../../assets/image/trafic/D1189.jpg">',
+    //     answer: "Je peux passer mais je doit ceder le passage aux conducteurs venant de gauche",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Le feux est rouge, je ne peux pas passer",
+    //         "Oui",
+    //         "Je peux passer mais je doit ceder le passage aux conducteurs venant de gauche"
+    //     ],
+    //     explanation : "La flèche verte m'indique que le feux est vert pour pouvoir tourner a droite mais je n'ai pas pour autant priorité sur les conducteurs en face de moi."
     // },
     // {
     //     numb: 19,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Je souhaite tourner a gauche, puis-je le faire ?",
+    //     image : '<img src="../../assets/image/trafic/3331.jpg">',
+    //     answer: "Non, je sors d'un chemin de terre",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Non, je sors d'un chemin de terre",
+    //         "Oui, j'ai la priorité de droite",
+    //     ],
+    //     explanation : "Je n'ai aucune priorité lorsque je sors d'un chemin de terre."
     // },
     // {
     //     numb: 20,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "je doit me mettre sur la bande de droite pour pouvoir avancer ?",
+    //     image : '<img src="../../assets/image/trafic/3484.jpg">',
+    //     answer: "Non",  
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
-    // },
+    //         "Oui",
+    //         "Non",
+    //     ],
+    //     explanation : "Le camion affiche un panneau qui m'indique que je peux rester dérrière lui. Cela signifie surement qu'il n'est pas a l'arrêt"
+    // }, 
     // {
     //     numb: 21,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Il est obligatoire d'avoir dans son véhicule un exctincteur ?",
+    //     image : '<img src="../../assets/image/trafic/ADRCITER04D086.jpg">',
+    //     answer: "Oui, et il ne doit pas être trop vieux (bonne date)",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Non",
+    //         "Oui, et il ne doit pas être trop vieux (bonne date)",
+    //     ],
+    //     explanation : "L'exctincteur est un élément obligatoire dans son véhicule"
     // },
     // {
     //     numb: 22,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Je voudrais dépasser ce camion, je peux le faire ?",
+    //     image : '<img src="../../assets/image/trafic/B02D04.jpg">',
+    //     answer: "Non",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Oui",
+    //         "Non",
+    //         "Oui mais en respectant la vitesse maximale autorisée"
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 23,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Je suis placé correctement sur la route ?",
+    //     image : '<img src="../../assets/image/trafic/3591.jpg">',
+    //     answer: "Non",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Non",
+    //         "Oui",
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 24,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Je circule en region wallonne, quelle est la vitesse maximale autorisée au-delà de ce panneau ?",
+    //     image : '<img src="../../assets/image/trafic/3587.jpg">',
+    //     answer: "90 km/h",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "70 km/h",
+    //         "90 km/h",
+    //         "50 km/h"
+    //     ],
+    //     explanation : "Le panneau m'indique que je sors d'une aglomération et comme je circule en région wallonne la vitesse maximale autorisée est de 90 km/h."
     // },
     // {
     //     numb: 25,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 5,
+    //     question : "j'approche a ce carrefour, puis-je continuer ma route ?",
+    //     image : '<img src="../../assets/image/trafic/B04D27.jpg">',
+    //     answer: "Non, il faut laisser la voiture a ma droite passer",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Oui, le véhicule semble être à l'arrêt",
+    //         "Oui je suis prioritaire pour pouvoir passer",
+    //         "Non, la voiture a ma droite est prioritaire"
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 26,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 1,
+    //     question : "Le véhicule en face de moi est-il bien placé pour être en stationnement ?",
+    //     image : '<img src="../../assets/image/trafic/B02D37.jpg">',
+    //     answer: "Non, il doit être en deçà du panneau de stationnement",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Non, il doit être en deçà du panneau de stationnement",
+    //         "Oui il est correctement placé",
+    //         "Non, il ne peut pas se mettre en stationnement sur un trottoir"
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 27,
-    //     question : "une question ?",
-    //     answer: "2eme reponse",
+    //     points : 5,
+    //     question : "Le véhicule est entrain d'attendre en face de moi, puis-je passer ?",
+    //     image : '<img src="../../assets/image/trafic/B764.jpg">',
+    //     answer: "Non, je ne suis pas prioritaire",
     //     options: [
-    //         "1ere reponse stv",
-    //         "2eme reponse",
-    //         "3eme reponse"
-    //     ]
+    //         "Non, je ne suis pas prioritaire",
+    //         "Oui, le véhicule est entrain d'attendre pour me laisser passer",
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 28,
+    //     points : 1,
     //     question : "une question ?",
+    //     image : '<img src="../../assets/image/trafic/B02D37.jpg">',
     //     answer: "2eme reponse",
     //     options: [
     //         "1ere reponse stv",
     //         "2eme reponse",
     //         "3eme reponse"
-    //     ]
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 29,
+    //     points : 1,
     //     question : "une question ?",
+    //     image : '<img src="../../assets/image/trafic/B02D37.jpg">',
     //     answer: "2eme reponse",
     //     options: [
     //         "1ere reponse stv",
     //         "2eme reponse",
     //         "3eme reponse"
-    //     ]
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 30,
+    //     points : 1,
     //     question : "une question ?",
+    //     image : '<img src="../../assets/image/trafic/B02D37.jpg">',
     //     answer: "2eme reponse",
     //     options: [
     //         "1ere reponse stv",
     //         "2eme reponse",
     //         "3eme reponse"
-    //     ]
+    //     ],
+    //     explanation : ""
     // },
     // {
     //     numb: 31,
     //     question : "une question ?",
+    //     image : '<img src="../../assets/image/trafic/B02D37.jpg">',
     //     answer: "2eme reponse",
     //     options: [
     //         "1ere reponse stv",
     //         "2eme reponse",
     //         "3eme reponse"
-    //     ]
+    //     ],
     // },
     // {
     //     numb: 32,
     //     question : "une question ?",
+    //     image : '<img src="../../assets/image/trafic/B02D37.jpg">',
     //     answer: "2eme reponse",
     //     options: [
     //         "1ere reponse stv",
