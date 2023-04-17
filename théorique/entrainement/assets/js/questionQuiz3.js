0d878eef5bdcce3699b289c37528d034bdc73502
curlyZafir <109031346+curlyZafir@users.noreply.github.com>
2023-04-17 18:39:22 +0200

    question quiz3

---
+++ théorique/entrainement/assets/js/questionQuiz3.js
@@ -0,0 +1,36 @@
+let questions = [
+
+    {
+        numb: 1,
+        points : 1,
+        question : "Aux places arrière, le port de la ceinture de sécurité est :",
+        image : '<img src="../../assets/image/entrainement-image/quiz1/quiz1_image1.jfif">',
+        answer: "obligatoire",
+        options: [
+            "obligatoire",
+            "conseillé hors agglomération",
+            "déconseillé en ville"
+        ],
+        explanation : "L'obligation de porter la ceinture de sécurité pour tout déplacement vise toutes les personnes (conducteurs et passagers avant/arrière) assises aux places équipées de ceintures."
+    },
+
+
+    {
+        numb: 1,
+        points : 1,
+        question : "
+        Comment transporter, de jour, ces tubes de 6 m de long sur une voiture de 4 m de longueur ? Avec ce panneau spécial accroché à l'arrière, je fais dépasser :",
+        image : '<img src="../../assets/image/entrainement-image/quiz1/quiz1_image1.jfif">',
+        answer: "obligatoire",
+        options: [
+            "obligatoire",
+            "conseillé hors agglomération",
+            "déconseillé en ville"
+        ],
+        explanation : "L'obligation de porter la ceinture de sécurité pour tout déplacement vise toutes les personnes (conducteurs et passagers avant/arrière) assises aux places équipées de ceintures."
+    },
+
+
+
+
+]
