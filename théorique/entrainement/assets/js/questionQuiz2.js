31c81bc3e280d754b0eeab258d4a8cca7ce9f9c4
curlyZafir <109031346+curlyZafir@users.noreply.github.com>
2023-04-23 11:50:52 +0200

    css du quiz

--- théorique/entrainement/assets/js/questionQuiz2.js
+++ théorique/entrainement/assets/js/questionQuiz2.js
@@ -152,4 +152,227 @@ let questions = [
         ],
         explanation : "Après une conduite malgré un retrait immédiat ou une déchéance, le Juge est obligé de prononcer une déchéance du droit de conduire. INFRACTION TRÈS GRAVE !"
     },
+
+    {
+        numb: 11,
+        points : 1,
+        question : "Ces petits signaux lumineux s’adressent :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image11.jfif">',
+        answer: "aux conducteurs",
+        options: [
+            "aux conducteurs",
+            "aux piétons",
+            "aux deux-roues"
+        ],
+        explanation : "Pour la facilité, des signaux lumineux peuvent être répétés sur le même poteau à hauteur du conducteur."
+    },
+
+    {
+        numb: 12,
+        points : 1,
+        question : "Ce signal annonce :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image12.jfif">',
+        answer: "des virages dangereux",
+        options: [
+            "une chaussée glissante",
+            "des virages dangereux",
+            "une succession de virages dont le premier à droite"
+        ],
+        explanation : "Ce signal de danger (A1c) annonce une succession d'au moins deux virages dangereux, le premier à gauche."
+    },
+
+    {
+        numb: 13,
+        points : 1,
+        question : "Quelle combinaison de signaux indique « abords d'école » :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image13.jfif">',
+        answer: "les signaux 1 (à gauche)",
+        options: [
+            "les signaux 1 (à gauche)",
+            "les signaux 2 (au centre)",
+            "les signaux 3 (à droite)"
+        ],
+        explanation : "Le signal F4a (zone 30) associé au signal A23 (endroit spécialement fréquenté par des enfants) délimite une zone constituée d'une ou de plusieurs voies publiques, ou parties de voie publique, incluant un accès à une école, où la vitesse est limitée à 30 km/h en permanence, de jour comme de nuit, même pendant les congés scolaires, ou seulement lorsque le signal F4a est allumé."
+    },
+
+    {
+        numb: 14,
+        points : 1,
+        question : "Ce signal annonce un carrefour :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image14.jfif">',
+        answer: "où la priorité de droite est applicable",
+        options: [
+            "où la priorité de droite est applicable",
+            "dangereux à 150 m",
+            "dangereux à 150 m où la priorité de droite est applicable"
+        ],
+        explanation : "Bien que sa forme triangulaire fasse songer à un signal de danger (placé généralement à 150 m), il s'agit d'un signal réglant la priorité. Ce signal (B17) annonce un carrefour où la priorité de droite est applicable."
+    },
+
+    {
+        numb: 15,
+        points : 1,
+        question : "Cette signalisation interdit l'accès aux conducteurs de :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image15.jfif">',
+        answer: "motocyclettes",
+        options: [
+            "motocyclettes",
+            "cyclomoteurs classe B et motocyclettes",
+            "cyclomoteurs (classe A et B) et motocyclettes"
+        ],
+        explanation : "Ce signal (C7) interdit l'accès aux conducteurs de motocyclettes (avec ou sans side-car)."
+    },
+
+    {
+        numb: 16,
+        points : 1,
+        question : "Ce signal :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image16.jfif">',
+        answer: "interdit aux conducteurs d'autocar d'emprunter cette route",
+        options: [
+            "oblige les conducteurs d'autocar à emprunter cette route",
+            "interdit aux conducteurs d'autocar d'emprunter cette route",
+            "indique une route spécialement réservée aux autocars"
+        ],
+        explanation : "Ce signal (C22) interdit l'accès aux conducteurs d'autocars."
+    },
+
+    {
+        numb: 17,
+        points : 1,
+        question : "Un conducteur fait l’objet d’un retrait immédiat du permis de conduire. Il ne trouve aucun proche pour venir le rechercher. Après le départ de la police, il reprend le volant mais tombe sur un nouveau contrôle. Le juge :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image17.jfif">',
+        answer: "doit prononcer une déchéance du droit de conduire",
+        options: [
+            "peut prononcer une déchéance du droit de conduire",
+            "doit prononcer une déchéance du droit de conduire",
+            "ne peut pas prononcer une déchéance du droit de conduire"
+        ],
+        explanation : "Après une conduite malgré un retrait immédiat ou une déchéance, le Juge est obligé de prononcer une déchéance du droit de conduire. INFRACTION TRÈS GRAVE !"
+    },
+
+    {
+        numb: 18,
+        points : 1,
+        question : "Ce signal m'indique :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image18.jfif">',
+        answer: "le début d'une interdiction d'arrêt et de stationnement",
+        options: [
+            "le début d'une interdiction de stationnement",
+            "le début d'une interdiction d'arrêt et de stationnement",
+            "la fin d'une interdiction d'arrêt et de stationnement"
+        ],
+        explanation : "Ce signal (E3) interdit l'arrêt et le stationnement. La flèche vers le haut indique le début de la réglementation."
+    },
+
+    {
+        numb: 19,
+        points : 1,
+        question : "Ce signal annonce :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image19.jfif">',
+        answer: "une zone résidentielle",
+        options: [
+            "une rue piétonnière",
+            "une rue réservée aux jeux d'enfants en période de vacances",
+            "une zone résidentielle"
+        ],
+        explanation : "Ce signal (F12a) indique le commencement d'une zone résidentielle ou de rencontre."
+    },
+
+    {
+        numb: 20,
+        points : 1,
+        question : "Ce signal annonce :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image20.jfif">',
+        answer: "doit prononcer une déchéance du droit de conduire",
+        options: [
+            "une course cycliste",
+            "une piste cyclable",
+            "un passage pour 2 roues (cycles et cyclos)"
+        ],
+        explanation : "Ce signal (F50) localise l'endroit où vous allez rencontrer un passage pour conducteurs de bicyclettes et de cyclomoteurs à deux roues (à proximité immédiate) qui vous aura été signalé à environ 150 m par un signal de danger (A25)."
+    },
+
+    {
+        numb: 21,
+        points : 1,
+        question : "Une rue cyclable est exclusivement réservée à la circulation des cyclistes :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image21.jfif">',
+        answer: "non",
+        options: [
+            "oui",
+            "non"
+        ],
+        explanation : "Ce signal (F111) indique le début d’une rue cyclable. Elle n’est pas exclusivement réservée à la circulation des cyclistes, les autres conducteurs y sont admis, mais sans pouvoir dépasser les cyclistes, ni circuler à plus de 30 km/h."
+    },
+
+    {
+        numb: 22,
+        points : 1,
+        question : "Cette limitation de vitesse est en vigueur :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image22.jfif">',
+        answer: "sur l'ensemble des voiries de cette agglomération",
+        options: [
+            "jusqu'au prochain carrefour inclus",
+            "jusqu'à un signal de fin d'interdiction",
+            "sur l'ensemble des voiries de cette agglomération"
+        ],
+        explanation : "Le signal C43 avec la mention 30 km/h placé au-dessus du signal F1a ou F1b vaut sur l'ensemble des voiries de l'agglomération."
+    },
+
+    {
+        numb: 23,
+        points : 1,
+        question : "Lorsque le feu est rouge, je marque l’arrêt à hauteur :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image23.jfif">',
+        answer: "de la ligne blanche transversale",
+        options: [
+            "de la ligne blanche transversale",
+            "du feu",
+            "au choix"
+        ],
+        explanation : "Une ligne d'arrêt constituée par une ligne blanche continue, tracée perpendiculairement au bord de la chaussée, indique l'endroit où les conducteurs doivent marquer l'arrêt imposé par un signal B5 ou un signal lumineux de circulation. Si cette ligne est tellement en retrait, c’est pour une raison que vous ne pouvez apercevoir : des bus venant de gauche tournent dans cette rue et doivent se déporter sur cette partie de la chaussée."
+    },
+
+    {
+        numb: 24,
+        points : 1,
+        question : "Des travaux entravent ma progression. La signalisation m'autorise à m'engager :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image24.jfif">',
+        answer: "oui",
+        options: [
+            "oui",
+            "oui, en cédant le passage aux conducteurs venant en sens inverse",
+            "non"
+        ],
+        explanation : "Devant deux signaux contradictoires (le feu vert et le signal B19 qui m'indique un passage étroit avec obligation de céder le passage aux conducteurs venant en sens inverse), le plus « souple » l'emporte, à savoir les feux lumineux sur les signaux routiers. Les conducteurs venant en sens inverse sont arrêtés par un feu rouge."
+    },
+
+    {
+        numb: 25,
+        points : 1,
+        question : "À 2h du matin, après avoir consommé de l'alcool, mon taux d'alcoolémie est de 1,5 g/l de sang (0,66 mg/l d'air expiré). Après 6 heures de sommeil, à 8h, mon taux sera de :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image25.jfif">',
+        answer: "0,6 g/l de sang (0,3 mg/l d'air expiré)",
+        options: [
+            "0,3 g/l de sang (0,15 mg/l d'air expiré)",
+            "0,6 g/l de sang (0,3 mg/l d'air expiré)",
+            "0,9 g/l de sang (0,45 mg/l d'air expiré)"
+        ],
+        explanation : "On élimine en moyenne 0,15 g/l de sang (0,07 mg/l d'air expiré) par heure. Le sommeil ne modifie pas l'élimination de l'alcool. En 6 heures = 6 x 0,15 = 0,9 g/l de sang. Soustraction : 1,5 - 0,9 = 0,6 g/l, encore trop pour conduire !"
+    },
+
+    {
+        numb: 26,
+        points : 1,
+        question : "Après avoir engagé une vitesse, je dois progressivement :",
+        image : '<img src="../../assets/image/entrainement-image/quiz2/quiz2_image26.jfif">',
+        answer: "embrayer",
+        options: [
+            "embrayer",
+            "débrayer"
+        ],
+        explanation : "Avant d'engager une vitesse, il faut « débrayer » pour désolidariser les roues du moteur. Après avoir engagé une vitesse, il faut progressivement relâcher la pédale ou le levier d'embrayage, c'est-à-dire : « embrayer »."
+    },
+
 ]
